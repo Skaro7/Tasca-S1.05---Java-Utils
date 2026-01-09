051939f0185884f11aacf3cacdbd64ca2ef2e9ba
Skaro7 <marc_tau72@hotmail.com>
2026-01-09 12:30:04 +0100

    feat: list directory files sorted by name

---
+++ src/main/java/Exercise1/DirectoryLister.java
@@ -0,0 +1,56 @@
+package Exercise1;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class DirectoryLister {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String directoryPath = sc.nextLine().trim();
+
+        File directory = new File(directoryPath);
+
+        System.out.println("Enter a directory path");
+
+        //Dosn't exist
+        if (!directory.exists()) {
+            System.out.println("Directory does not exist");
+            sc.close();
+            return;
+        }
+
+        //Is not a directory
+        if (!directory.isDirectory()) {
+            System.out.println("Path is not a directory");
+            sc.close();
+            return;
+        }
+
+        File[] files = directory.listFiles();
+
+        if (files == null) {
+            System.out.println("Error reading directory");
+            sc.close();
+            return;
+        }
+
+        Arrays.sort(files);
+
+        if (files != null) {
+            Arrays.sort(files, new Comparator<File>() {
+                @Override
+                public int compare(File o1, File o2) {
+                    return o1.getName().compareTo(o2.getName());
+                }
+            });
+
+            for (File file : files) {
+                System.out.println(file.getName());
+            }
+
+            sc.close();
+        }
+    }
+}
